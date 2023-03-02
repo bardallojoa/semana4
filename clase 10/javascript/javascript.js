9db83309c42ec33f6ac043b8c2684aea29b40b9e
bardallojoa <124754207+bardallojoa@users.noreply.github.com>
2023-03-02 15:11:36 -0300

    Add files via upload

---
+++ clase 10/javascript/javascript.js
@@ -0,0 +1,14 @@
+let nombre = "joaquin ";
+let apellido = "bardallo";
+let resultado = nombre + apellido;
+console.log (resultado);
+
+
+
+let dia = 1;
+let mes = 3;
+let suma = dia + mes;
+console.log (suma);
+let anio = 2023;
+let suma2 = suma + anio;
+console.log (suma2);
